50759ead587a743d3c6855bfbc5b69ef52be4fbb
hycoding51 <yanghuan.bnu@gmail.com>
2019-12-12 18:42:09 -0500

    search recipe by cusine

--- js/app.js
+++ js/app.js
@@ -9,4 +9,33 @@ recipeSearchBtn.addEventListener("click", function(event) {
   var selectedCuisine = cuisineSelectInput.options[i].text;
 
   console.log(selectedCuisine);
+
+  // search in spoonacular API and print on html
+  const spoonacularKey = "c3d4ddbeedc2444a984cddbbc42db308";
+  var queryURL = `https://api.spoonacular.com/recipes/search?cuisine=${selectedCuisine}&number=5&apiKey=${spoonacularKey}`;
+  $.ajax({
+    type: "GET",
+    url: queryURL
+    // response headers: {
+    //   "Content-Type": "application/json"
+    // }???
+  }).then(function(response) {
+    console.log(response);
+    recipeId = response.results[0].id;
+    console.log(recipeId);
+    var baseURL = "https://spoonacular.com/recipeImages/";
+    var imgURL = baseURL + response.results[0].image;
+    // var dishImg = $("<img>").attr("src", imgURL);
+    // $(".images").append(dishImg);
+    var dishTitle = $("<td>").text(response.results[0].title);
+    $("<tr>").append(dishTitle);
+    var prepareMinutes = $("<h3>").text(
+      `Ready in ${response.results[0].readyInMinutes} minutes`
+    );
+    $(".info").append(prepareMinutes);
+    var totalServings = $("<h3>").text(
+      `Servings: ${response.results[0].servings}`
+    );
+    $(".info").append(totalServings);
+  });
 });
