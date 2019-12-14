e24cff961c6ffc835461b5808daeb1f598d885b5
Edward Harley <edharley19@gmail.com>
2019-12-14 12:41:48 -0500

    update: Update api key in app.js

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ recipeSearchBtn.addEventListener("click", function(event) {
   console.log(selectedCuisine);
 
   // search in spoonacular API and print on html
-  const spoonacularKey = "c3d4ddbeedc2444a984cddbbc42db308";
+  const spoonacularKey = "0d9362f34041402b8ec15c03ed3dead9";
   var queryURL = `https://api.spoonacular.com/recipes/search?cuisine=${selectedCuisine}&number=5&apiKey=${spoonacularKey}`;
   $.ajax({
     type: "GET",
@@ -27,11 +27,11 @@ recipeSearchBtn.addEventListener("click", function(event) {
       var recipeId = results[i].id;
       var recipeTitle = results[i].title;
       var img = `<img class="resultImg" src="https://spoonacular.com/recipeImages/${response.results[i].image}">`;
-      //   var baseURL = "https://spoonacular.com/recipeImages/";
-      //   var recipeImgURL = baseURL + response.results[i].image;
-      //   var recipeImg = $("<img>");
-      //   recipeImg.attr("src", recipeImgURL);
-      //   recipeImg.attr("class", "resultImg");
+        var baseURL = "https://spoonacular.com/recipeImages/";
+        var recipeImgURL = baseURL + response.results[i].image;
+        var recipeImg = $("<img>");
+        recipeImg.attr("src", recipeImgURL);
+        recipeImg.attr("class", "resultImg");
       var likeUnlike = currentUser.recipes.indexOf(recipeId) === -1 ? "like" : "unlike";
       var likeButton = `<button id=${recipeId} type="button" class="like-button" state="${likeUnlike}">${likeUnlike.toUpperCase()}</button>`;
       var recipeServing = results[i].servings;
@@ -46,21 +46,21 @@ recipeSearchBtn.addEventListener("click", function(event) {
       $("tbody").append(newRow);
 
       console.log("Recipe ID is" + recipeId);
-      //   var recipeURL =
-      //     `https://api.spoonacular.com/recipes/${recipeId}/information?includeNutrition=false&apiKey=` +
-      //     spoonacularKey;
-      //   $.ajax({
-      //     type: "GET",
-      //     url: recipeURL
-      //   }).then(function(response) {
-      //     console.log(response);
-      //     var allIngrdts = response.extendedIngredients;
-      //     console.log(allIngrdts);
-      //     for (var i = 0; i < allIngrdts.length; i++) {
-      //       var ingredient = $("<h3>").text(allIngrdts[i].originalString);
-      //       $(".ingredients").append(ingredient);
-      //     }
-      //   });
+        var recipeURL =
+          `https://api.spoonacular.com/recipes/${recipeId}/information?includeNutrition=false&apiKey=` +
+          spoonacularKey;
+        $.ajax({
+          type: "GET",
+          url: recipeURL
+        }).then(function(response) {
+          console.log(response);
+          var allIngrdts = response.extendedIngredients;
+          console.log(allIngrdts);
+          for (var i = 0; i < allIngrdts.length; i++) {
+            var ingredient = $("<h3>").text(allIngrdts[i].originalString);
+            $(".ingredients").append(ingredient);
+          }
+        });
       console.log(recipeArr);
       // get ingredients
     }
