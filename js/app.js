1b9a992bc088f1cd9201d5fd82bb62d56de93127
Edward Harley <edharley19@gmail.com>
2019-12-14 15:22:47 -0500

    feat: Add feature to display saved recipes in firestore to saved.html

--- js/app.js
+++ js/app.js
@@ -107,29 +107,3 @@ recipeSearchBtn.addEventListener("click", function(event) {
 
   });
 });
-
-var firebaseConfig = {
-  apiKey: "AIzaSyBf1Ulnb618Uos69ZB7Ti0mZ6tUev842xk",
-  authDomain: "project-one-b8f55.firebaseapp.com",
-  databaseURL: "https://project-one-b8f55.firebaseio.com",
-  projectId: "project-one-b8f55",
-  storageBucket: "project-one-b8f55.appspot.com",
-  messagingSenderId: "558983366534",
-  appId: "1:558983366534:web:a71ad15fe65c9ac9964e07"
-};
-
-firebase.initializeApp(firebaseConfig);
-var db = firebase.firestore();
-const userRef = db.collection("users").doc("LCaIJje6quQOd5NOc9Cw")
-userRef.onSnapshot(doc => {
-  if (doc.exists) {
-    var userData = doc.data();
-    console.log("[DEBUG] user LCaIJje6quQOd5NOc9Cw updated ::", userData);
-    currentUser = {
-      id: "LCaIJje6quQOd5NOc9Cw",
-      ...userData
-    }
-  } else {
-    console.warn("[WARNING] user LCaIJje6quQOd5NOc9Cw does not exist");
-  }
-});
--- js/firebase.js
+++ js/firebase.js
@@ -1,43 +1,30 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBf1Ulnb618Uos69ZB7Ti0mZ6tUev842xk",
+  authDomain: "project-one-b8f55.firebaseapp.com",
+  databaseURL: "https://project-one-b8f55.firebaseio.com",
+  projectId: "project-one-b8f55",
+  storageBucket: "project-one-b8f55.appspot.com",
+  messagingSenderId: "558983366534",
+  appId: "1:558983366534:web:a71ad15fe65c9ac9964e07"
+};
 
-const qs = selector => document.querySelector(selector);
-// references to our count elements
-const saved = qs(".saveButton");
-const text = qs(".idRecipe");
-var recipe=[262825]
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
 
-       // initialize state
-    //    var count = "262825";
-       // update UI and state on button click
-       saved.addEventListener("click", () => {
-         
-         text.textContent = text;
-         countRef
-           .update({
-             text: firebase.firestore.FieldValue.arrayUnion(recipe[0])
-           })
-           .then(() => console.log("updated the count successfully!"));
-       });
+var currentUser = {};
 
-       var firebaseConfig = {
-        apiKey: "AIzaSyBf1Ulnb618Uos69ZB7Ti0mZ6tUev842xk",
-        authDomain: "project-one-b8f55.firebaseapp.com",
-        databaseURL: "https://project-one-b8f55.firebaseio.com",
-        projectId: "project-one-b8f55",
-        storageBucket: "project-one-b8f55.appspot.com",
-        messagingSenderId: "558983366534",
-        appId: "1:558983366534:web:a71ad15fe65c9ac9964e07"
-      };
-      
-      firebase.initializeApp(firebaseConfig);
-      var db = firebase.firestore();
-      const countRef = db.collection("users").doc("6AqEslwCOj4wCVoNv1tM")
-      countRef.onSnapshot(doc => {
-        if (doc.exists) {
-          console.log("Document data:", doc.data());
-          count = doc.data().text;
-          text.textContent = count;
-        } else {
-          // doc.data() will be undefined in this case
-          console.warn("No such document!");
-        }
-      });
+const userRef = db.collection("users").doc("LCaIJje6quQOd5NOc9Cw")
+userRef.onSnapshot(doc => {
+  if (doc.exists) {
+    var userData = doc.data();
+    console.log("[DEBUG] user LCaIJje6quQOd5NOc9Cw updated ::", userData);
+    currentUser = {
+      id: "LCaIJje6quQOd5NOc9Cw",
+      ...userData
+    }
+  } else {
+    console.warn("[WARNING] user LCaIJje6quQOd5NOc9Cw does not exist");
+  }
+});
+
+
